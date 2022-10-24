ccd0f13bfc7cefd67bf4862944af1f343fca14eb
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-10-23 21:19:12 -0300

    components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import FlexShrink from './components/container';
 import './index.css';
-import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FlexShrink/>
   </React.StrictMode>
 );
+
+
 
 
