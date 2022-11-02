55f7546c8ff7c7803bf782b884410054eb547707
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-11-02 19:20:11 -0300

    render OrderSection

--- src/sections/OrderSection/OrderSection.jsx
+++ src/sections/OrderSection/OrderSection.jsx
@@ -1,7 +1,9 @@
 import { Container } from "@mui/material";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export default function OrderSection() {
- 
+export default function OrderSection({ cartItems }) {
+  //console.log(cartItems);
   return (
     <Container
       sx={{
@@ -14,11 +16,22 @@ export default function OrderSection() {
         height: "85vh",
       }}
     >
-      {
-        <div className="title-order">
-          <h3>Your order summary</h3>
+      {<p className="title-order">Your order summary</p>}
+      {cartItems.map(({ item, price }, index) => (
+        <div className="container-order" key={index}>
+          <div className="item-order-left">
+            <p>{item}</p>
+          </div>
+          <div>
+            <button className="btn-remove-food">-</button>
+            <input className="input-number" type="number" min="1"></input>
+            <button className="btn-add-food">+</button>
+          </div>
+          <div className="price-order">${price}</div>
+          <div className="icon-trash"> <FontAwesomeIcon icon={faTrash} /></div>
         </div>
-      }
+      ))}
     </Container>
   );
 }
+
